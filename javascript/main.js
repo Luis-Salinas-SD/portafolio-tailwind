e58e272e86738124679cf268439d0eb26814fe71
Luis-Salinas-SD <paus.impala21@gmail.com>
2021-05-01 19:12:54 -0500

    Actualizacion form funcional

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,19 @@
-const form = document.querySelector('#form');
-form.addEventListener('click', funcionClick);
+const formulario = document.querySelector('#formulario')
 
-function funcionClick(click) {
+formulario.addEventListener('submit', handleSubmit);
 
-}
+async function handleSubmit(event) {
+    event.preventDefault()
+    const form = new FormData(this)
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers: {
+            'Accept': 'application/json'
+        }
+    })//fetch
+    if (response.ok) {
+        alert('¡¡¡ Gracias por contacatarme, espera mi respuesta pronto !!!');
+        this.reset()
+    }//if
+}//function
